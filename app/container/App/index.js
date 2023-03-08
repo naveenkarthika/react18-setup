3bdfcc2e66b6298a6b8f3d755fecd7bf003a06a7
naveenkumar <a.naveenkumar.rifluxyss@gmail.com>
2023-03-08 21:20:17 +0530

    pre-commit added

--- app/container/App/index.js
+++ app/container/App/index.js
@@ -1,23 +1,29 @@
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-filename-extension */
-import React from "react";
+import React from 'react';
 
 function App() {
   const data = {
     name: {
-      key: "tset",
+      key: 'tset',
     },
   };
 
   console.log(data?.name?.key);
   return (
-  <div>
-    <p>Nostrud quis reprehenderit amet aliquip occaecat eiusmod. Incididunt fugiat consequat occaecat pariatur. Commodo aute enim tempor aliqua aute id veniam deserunt adipisicing nulla aliquip aliqua. In ad exercitation Lorem enim in veniam ut tempor eiusmod nisi in sint. Aliqua ullamco enim ad anim veniam magna sint laboris laboris. Aliqua occaecat esse magna ut nulla laboris exercitation tempor anim. Ad anim cupidatat exercitation aute ex amet ex est enim aliquip ea.</p>
-  <h2>Hello world!!</h2>
-  </div>
-  )
+    <div>
+      <p>
+        Nostrud quis reprehenderit amet aliquip occaecat eiusmod. Incididunt
+        fugiat consequat occaecat pariatur. Commodo aute enim tempor aliqua aute
+        id veniam deserunt adipisicing nulla aliquip aliqua. In ad exercitation
+        Lorem enim in veniam ut tempor eiusmod nisi in sint. Aliqua ullamco enim
+        ad anim veniam magna sint laboris laboris. Aliqua occaecat esse magna ut
+        nulla laboris exercitation tempor anim. Ad anim cupidatat exercitation
+        aute ex amet ex est enim aliquip ea.
+      </p>
+      <h2>Hello world!!</h2>
+    </div>
+  );
 }
-
-
 
 export default App;
