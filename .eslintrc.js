08dfe79612a78499f9d4f4cbb2ad51dd609f0c43
naveenkumar <a.naveenkumar@rifluxyss.com>
2023-03-09 12:48:26 +0530

    Eslint & prettier config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const prettierOptions = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, ".prettierrc"), "utf8"),
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
 );
 
 module.exports = {
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "prettier"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier',
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: [
-    "react",
-    "prettier",
-    "prettier/react"
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": ["error", prettierOptions],
-    "no-param-reassign": ["error", { props: false }],
+    'prettier/prettier': ['error', prettierOptions],
+    'no-param-reassign': ['error', { props: false }],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
   },
 };
